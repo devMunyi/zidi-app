5353f15d4da5ce5ddefa1c7153adef79f5eaf70d
devMunyi <samunyi90@gmail.com>
2022-04-22 16:16:09 +0300

    Update

--- index.php
+++ index.php
@@ -117,21 +117,17 @@ include_once("configs/conn.inc");
                         <div class="col-lg-9 col-md-9 pb-1 pt-1">
                             <table>
                                 <tr>
-                                    <td style="width: 230px;" class="pt-1">
+                                    <td style="width: 300px;" class="pt-1">
                                         <!-- <span class="font-italic"> An OOP Implementation by Jonah Ngarama </span> -->
                                         <h6 class="font-italic" id="imptype-and-contributor"> </h6>
                                     </td>
-                                    <td class="text-center" id="language-dropdown">
+                                    <!-- <td class="text-center" id="language-dropdown">
 
-                                    </td>
+                                    </td> -->
 
                                     <td class="text-center" id="framework-dropdown">
 
                                     </td>
-
-                                    <!-- <td class="text-center" id="version-dropdown">
-                                    
-                                    </td> -->
                                 </tr>
                             </table>
 
@@ -143,40 +139,48 @@ include_once("configs/conn.inc");
                         </div>
                     </div>
                 </div>
-                <div class="row clearfix row-deck" style="min-height: 600px; width: 99%;">
-                    <div class="col-lg-2 col-md-2 pl-0 pr-0">
-                        <div class="scrollk">
-                            <div class="transcard">
-                                <div class="card-header pl-0 pt-3 pb-1 pr-0">
-                                    <h4 class="card-title text-orange"><i class="fe fe-droplet"></i> Language</h4>
-                                </div>
-                                <div class="card-body p-0">
-                                    <ul class="metismenu ci-effect-1 prominent lang_" id="language_">
-                                        <li class="font-weight-normal font-14 font-italic">Loading...</li>
-                                    </ul>
+                <div class="row" style="min-height: 400px;">
+                    <div class="col-lg-8 col-md-8">
+                        <div class="row">
+                            <div class="col-lg-3 col-md-3 pl-2 pr-0">
+                                <div class="scrollk">
+                                    <div class="transcard">
+                                        <div class="card-header pl-0 pt-3 pb-1 pr-0">
+                                            <h4 class="card-title text-orange"><i class="fe fe-droplet"></i> Language</h4>
+                                        </div>
+                                        <div class="card-body p-0">
+                                            <ul class="metismenu ci-effect-1 prominent lang_" id="language_">
+                                                <li class="font-weight-normal font-14 font-italic">Loading...</li>
+                                            </ul>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-6 col-md-6">
-                        <div class="card transcard ">
-                            <div class="card-header pb-2 pt-2">
-                                <table style="width: 100%">
-                                    <tr>
-                                        <td>
-                                            <h3 class="card-title"><i title="code" class="fe fe-code"></i> <!--Code Snippet--> </h3>
-                                        </td>
-                                        <td>
-                                            
-                                            <button class="btn text-white btn-sm text-sm bg-indigo mr-3"> <i title="copy" class="fe fe-copy"></i> <!--Copy--> </button>
-                                            <button class="btn text-white btn-sm text-sm mr-3" style="background: forestgreen;"> <i title="download" class="fe fe-download"></i> <!--Download--> </button>
-                                            <button class="btn btn-dark btn-sm text-sm"> <i title="share" class="fe fe-share-2"></i> <!--Share--> </button>
-                                        </td>
-                                    </tr>
-
-                                </table>
-                                <div class="card-options">
-                                    <!--
+                            <div class="col-lg-9 col-md-9 pl-2 pr-0">
+                                <div class="card transcard ">
+                                    <div class="card-header pb-2 pt-2">
+                                        <table style="width: 100%">
+                                            <tr>
+                                                <td>
+                                                    <h3 class="card-title"><i title="code" class="fe fe-code"></i>
+                                                        <!--Code Snippet-->
+                                                    </h3>
+                                                </td>
+                                                <td>
+                                                    <button class="btn text-white btn-sm text-sm bg-indigo mr-3"> <i title="copy" class="fe fe-copy"></i>
+                                                        <!--Copy-->
+                                                    </button>
+                                                    <button class="btn text-white btn-sm text-sm mr-3" style="background: forestgreen;"> <i title="download" class="fe fe-download"></i>
+                                                        <!--Download-->
+                                                    </button>
+                                                    <button class="btn btn-dark btn-sm text-sm"> <i title="share" class="fe fe-share-2"></i>
+                                                        <!--Share-->
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                        <div class="card-options">
+                                            <!--
                                     <input type="checkbox" id="checkbox" class="checkbox">
                                     <label for="checkbox" class="label">
                                         <i class="fas fa-moon"></i>
@@ -186,29 +190,31 @@ include_once("configs/conn.inc");
                                         </div>
                                     </label>
                                     `-->
-                                    <label class="custom-switch m-0">
-                                        <input type="checkbox" value="1" class="custom-switch-input" id="custom-switch-input" checked="">
-                                        <span class="custom-switch-indicator"></span>
-                                    </label>
-                                </div>
-                            </div>
-                            <div id="editor" class="card-body dark-screen">
+                                            <label class="custom-switch m-0">
+                                                <input type="checkbox" value="1" class="custom-switch-input" id="custom-switch-input" checked="">
+                                                <span class="custom-switch-indicator"></span>
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <div id="editor" class="card-body dark-screen">
 
-                            </div>
-                            <div class="card-footer">
-                                <div class="d-flex justify-content-between">
-                                    <table style="width: 70%;">
-                                        <tr>
-                                            <td><button class="btn bg-transparent text-black font-weight-bold"> <i class="fe fe-alert-triangle"></i> <label></label>Issues <span class="badge bg-dark">4</span></button></td>
-                                            <td><button class="btn bg-transparent text-black font-weight-bold"> <i class="fe fe-message-square"></i> Comments <span class="badge bg-dark">15</span></button></td>
-                                            <td><button class="btn bg-transparent text-black font-weight-bold"> <i class="fe fe-heart"></i> Likes <span class="badge bg-dark">120</span></button></td>
-                                        </tr>
-                                    </table>
+                                    </div>
+                                    <div class="card-footer">
+                                        <div class="d-flex justify-content-between">
+                                            <table style="width: 70%;">
+                                                <tr>
+                                                    <td><button class="btn bg-transparent text-black font-weight-bold"> <i class="fe fe-alert-triangle"></i> <label></label>Issues <span class="badge bg-dark">4</span></button></td>
+                                                    <td><button class="btn bg-transparent text-black font-weight-bold"> <i class="fe fe-message-square"></i> Comments <span class="badge bg-dark">15</span></button></td>
+                                                    <td><button class="btn bg-transparent text-black font-weight-bold"> <i class="fe fe-heart"></i> Likes <span class="badge bg-dark">120</span></button></td>
+                                                </tr>
+                                            </table>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div class="col-lg-4 col-md-4">
+                    <div class="col-lg-4 col-md-4" style="margin-left: 0; padding-left: 0; overflow-y: scroll; max-height: 500px;">
                         <div class="card transcard">
                             <div class="card-header pt-3">
                                 <h3 class="card-title"><i class="fe fe-eye"></i> Available Solutions</h3>
@@ -302,7 +308,7 @@ include_once("configs/conn.inc");
             functions_load() //load all functions
             load_languages(); /////Load all the languages
             loadCodesnippetsLink();
-            
+
 
             //load frameworks based on language selected
             //let current_loc = JSON.parse(localStorage.getItem("persist"));
