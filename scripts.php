bb5755e35c8709b9e774bbb6e7c3a94a81e5748f
devMunyi <samunyi90@gmail.com>
2022-07-15 11:40:34 +0300

    Update

--- scripts.php
+++ scripts.php
@@ -1,53 +1,53 @@
 <!-- jQuery 3 -->
-<script src="assets/plugins/jquery/jquery.min.js"></script>
+<script src="/assets/plugins/jquery/jquery.min.js"></script>
 
 <!-- Bootstrap-->
-<script src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+<script src="/assets/plugins/bootstrap/js/bootstrap.min.js"></script>
 
-<script src="assets/bundles/lib.vendor.bundle.js"></script>
+<script src="/assets/bundles/lib.vendor.bundle.js"></script>
 
 <!-- start plugin js file  -->
-<script src="assets/bundles/apexcharts.bundle.js"></script>
-<script src="assets/bundles/counterup.bundle.js"></script>
-<script src="assets/bundles/knobjs.bundle.js"></script>
-<script src="assets/bundles/c3.bundle.js"></script>
-<script src="assets/bundles/flot.bundle.js"></script>
-<script src="assets/bundles/jvectormap1.bundle.js"></script>
+<script src="/assets/bundles/apexcharts.bundle.js"></script>
+<script src="/assets/bundles/counterup.bundle.js"></script>
+<script src="/assets/bundles/knobjs.bundle.js"></script>
+<script src="/assets/bundles/c3.bundle.js"></script>
+<script src="/assets/bundles/flot.bundle.js"></script>
+<script src="/assets/bundles/jvectormap1.bundle.js"></script>
 
 <!-- Start core js and page js -->
-<script src="assets/js/core.js"></script>
-<script src="assets/js/custom/common.js"></script>
-<script src="assets/js/custom/authentication.js"></script>
-<script src="assets/js/custom/main.js"></script>
-<script src="assets/js/page/index.js"></script>
+<script src="/assets/js/core.js"></script>
+<script src="/assets/js/custom/common.js"></script>
+<script src="/assets/js/custom/authentication.js"></script>
+<script src="/assets/js/custom/main.js"></script>
+<script src="/assets/js/page/index.js"></script>
 
 
 <!-- SweetAlert2 -->
-<script src="assets/plugins/sweetalert2/sweetalert2.min.js"></script>
+<script src="/assets/plugins/sweetalert2/sweetalert2.min.js"></script>
 <!-- Toastr -->
-<script src="assets/plugins/toastr/toastr.min.js"></script>
+<script src="/assets/plugins/toastr/toastr.min.js"></script>
 
 
 <!--Required Ace Libralies-->
-<script src="assets/plugins/ace/ace-editor/src-min/ace.js"></script>
-<script src="assets/plugins/ace/ace-editor/src-min/mode-javascript.js"></script>
-<script src="assets/plugins/ace/ace-editor/src-min/ext-language_tools.js"></script>
-<!-- <script src=/assets/plugins/ace/ace-editor/src-min/ext-beautify.js"></script> -->
+<script src="/assets/plugins/ace/ace-editor/src-min/ace.js"></script>
+<script src="/assets/plugins/ace/ace-editor/src-min/mode-javascript.js"></script>
+<script src="/assets/plugins/ace/ace-editor/src-min/ext-language_tools.js"></script>
+<!-- <script src=//assets/plugins/ace/ace-editor/src-min/ext-beautify.js"></script> -->
 
 <!--Require Highlight js Library-->
-<!-- <script src=/assets/plugins/highlightjs/highlight.min.js"></script>
-<script src=/assets/plugins/highlightjs/highlightjs-line-numbers.min.js"></script>
+<!-- <script src=//assets/plugins/highlightjs/highlight.min.js"></script>
+<script src=//assets/plugins/highlightjs/highlightjs-line-numbers.min.js"></script>
 <script>hljs.highlightAll();</script>
 <script>hljs.initLineNumbersOnLoad();</script> -->
 
 <!--Custom script for the editor-->
-<script src="assets/plugins/ace/editor.js"></script>
-<script src="assets/plugins/ace/editor-console.js"></script>
-<script src="assets/js/jquery.mCustomScrollbar.concat.min.js"></script>
+<script src="/assets/plugins/ace/editor.js"></script>
+<script src="/assets/plugins/ace/editor-console.js"></script>
+<script src="/assets/js/jquery.mCustomScrollbar.concat.min.js"></script>
 <!-- Moment plugin script -->
-<script src="assets/plugins/moment/moment.min.js"></script>
+<script src="/assets/plugins/moment/moment.min.js"></script>
 
 <!-- Select2 JS --> 
-<script src="assets/plugins/select2/js/select2.min.js"></script>
+<script src="/assets/plugins/select2/js/select2.min.js"></script>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/js-beautify/1.6.8/beautify.js"></script>
