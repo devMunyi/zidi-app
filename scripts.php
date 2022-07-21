d2c413fecc59e0138a4c1128ba4198acfde1c04b
devMunyi <samunyi90@gmail.com>
2022-07-21 09:29:08 +0300

    styles and scripts path fix

--- scripts.php
+++ scripts.php
@@ -1,37 +1,37 @@
 <!-- jQuery 3 -->
-<script src="/zidi-app/assets/plugins/jquery/jquery.min.js"></script>
+<script src="/assets/plugins/jquery/jquery.min.js"></script>
 
 <!-- Bootstrap-->
-<script src="/zidi-app/assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+<script src="/assets/plugins/bootstrap/js/bootstrap.min.js"></script>
 
-<script src="/zidi-app/assets/bundles/lib.vendor.bundle.js"></script>
+<script src="/assets/bundles/lib.vendor.bundle.js"></script>
 
 <!-- start plugin js file  -->
-<script src="/zidi-app/assets/bundles/apexcharts.bundle.js"></script>
-<script src="/zidi-app/assets/bundles/counterup.bundle.js"></script>
-<script src="/zidi-app/assets/bundles/knobjs.bundle.js"></script>
-<script src="/zidi-app/assets/bundles/c3.bundle.js"></script>
-<script src="/zidi-app/assets/bundles/flot.bundle.js"></script>
-<script src="/zidi-app/assets/bundles/jvectormap1.bundle.js"></script>
+<script src="/assets/bundles/apexcharts.bundle.js"></script>
+<script src="/assets/bundles/counterup.bundle.js"></script>
+<script src="/assets/bundles/knobjs.bundle.js"></script>
+<script src="/assets/bundles/c3.bundle.js"></script>
+<script src="/assets/bundles/flot.bundle.js"></script>
+<script src="/assets/bundles/jvectormap1.bundle.js"></script>
 
 <!-- Start core js and page js -->
-<script src="/zidi-app/assets/js/core.js"></script>
-<script src="/zidi-app/assets/js/custom/common.js"></script>
-<script src="/zidi-app/assets/js/custom/authentication.js"></script>
-<script src="/zidi-app/assets/js/custom/main.js"></script>
-<script src="/zidi-app/assets/js/page/index.js"></script>
+<script src="/assets/js/core.js"></script>
+<script src="/assets/js/custom/common.js"></script>
+<script src="/assets/js/custom/authentication.js"></script>
+<script src="/assets/js/custom/main.js"></script>
+<script src="/assets/js/page/index.js"></script>
 
 
 <!-- SweetAlert2 -->
-<script src="/zidi-app/assets/plugins/sweetalert2/sweetalert2.min.js"></script>
+<script src="/assets/plugins/sweetalert2/sweetalert2.min.js"></script>
 <!-- Toastr -->
-<script src="/zidi-app/assets/plugins/toastr/toastr.min.js"></script>
+<script src="/assets/plugins/toastr/toastr.min.js"></script>
 
 
 <!--Required Ace Libralies-->
-<script src="/zidi-app/assets/plugins/ace/ace-editor/src-min/ace.js"></script>
-<script src="/zidi-app/assets/plugins/ace/ace-editor/src-min/mode-javascript.js"></script>
-<script src="/zidi-app/assets/plugins/ace/ace-editor/src-min/ext-language_tools.js"></script>
+<script src="/assets/plugins/ace/ace-editor/src-min/ace.js"></script>
+<script src="/assets/plugins/ace/ace-editor/src-min/mode-javascript.js"></script>
+<script src="/assets/plugins/ace/ace-editor/src-min/ext-language_tools.js"></script>
 <!-- <script src="/assets/plugins/ace/ace-editor/src-min/ext-beautify.js"></script> -->
 
 <!--Require Highlight js Library-->
@@ -41,13 +41,13 @@
 <script>hljs.initLineNumbersOnLoad();</script> -->
 
 <!--Custom script for the editor-->
-<script src="/zidi-app/assets/plugins/ace/editor.js"></script>
-<script src="/zidi-app/assets/plugins/ace/editor-console.js"></script>
-<script src="/zidi-app/assets/js/jquery.mCustomScrollbar.concat.min.js"></script>
+<script src="/assets/plugins/ace/editor.js"></script>
+<script src="/assets/plugins/ace/editor-console.js"></script>
+<script src="/assets/js/jquery.mCustomScrollbar.concat.min.js"></script>
 <!-- Moment plugin script -->
-<script src="/zidi-app/assets/plugins/moment/moment.min.js"></script>
+<script src="/assets/plugins/moment/moment.min.js"></script>
 
-<script src="/zidi-app/assets/plugins/beautify.js"></script>
+<script src="/assets/plugins/beautify.js"></script>
 
 <!-- Select2 JS --> 
-<script src="/zidi-app/assets/js/select2.min.js"></script>
+<script src="/assets/js/select2.min.js"></script>
