890853f1f1342b826d71a1f73f18e1242be5d385
Samuel Munyi <samunyi90@gmail.com>
2022-10-26 21:39:04 +0300

    Update scripts.php

--- scripts.php
+++ scripts.php
@@ -16,9 +16,9 @@
 
 <!-- Start core js and page js -->
 <script src="<?php echo $assets; ?>js/core.js"></script>
-<script src="<?php echo $assets; ?>js/custom/common.js?v9"></script>
-<script src="<?php echo $assets; ?>js/custom/authentication.js?v4"></script>
-<script src="<?php echo $assets; ?>js/custom/main.js?v22"></script>
+<script src="<?php echo $assets; ?>js/custom/common.js?v10"></script>
+<script src="<?php echo $assets; ?>js/custom/authentication.js?v5"></script>
+<script src="<?php echo $assets; ?>js/custom/main.js?v23"></script>
 <script src="<?php echo $assets; ?>js/page/index.js"></script>
 
 <!-- SweetAlert2 -->
