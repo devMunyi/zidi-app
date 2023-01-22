c5cfeffe9bdea521a4d9a89f5dcbbd0e803abc80
Samuel Munyi <samunyi90@gmail.com>
2023-01-22 20:35:01 +0300

    Update scripts.php

--- scripts.php
+++ scripts.php
@@ -18,7 +18,7 @@
 <script src="<?php echo $assets; ?>js/core.js"></script>
 <script src="<?php echo $assets; ?>js/custom/common.js?v12"></script>
 <script src="<?php echo $assets; ?>js/custom/authentication.js?v6"></script>
-<script src="<?php echo $assets; ?>js/custom/main.js?v35"></script>
+<script src="<?php echo $assets; ?>js/custom/main.js?v36"></script>
 <script src="<?php echo $assets; ?>js/page/index.js"></script>
 
 <!-- SweetAlert2 -->
